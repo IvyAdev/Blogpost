653f7093afee6c0a3c258f763e0a1755fdcd8e48
IvyAdev <anyangoivy1@gmail.com>
2025-05-13 06:15:28 -0700

    Add content to HTML, CSS, and JavaScript files

---
+++ blogpost.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const blogSection = document.querySelector(".latest-blog");
+  blogSection.addEventListener("click", () => {
+    alert("Thanks for reading our blog!");
+  });
+});
